7b3c8caa89519417576a53f68276d109c8aeceba
Joel van Ingen <j.c.ingen@gmail.com>
2026-02-26 13:36:25 +0100

    add tasktoken and paymentrequestid to payment service response

--- fake-services-nodejs/src/payment.ts
+++ fake-services-nodejs/src/payment.ts
@@ -4,13 +4,30 @@ import { v4 as uuidv4 } from "uuid";
 const sqsClient = new SQSClient({});
 const QUEUE_URL = process.env.PAYMENT_RESPONSE_QUEUE_URL;
 
+/**
+ * - Receives: taskToken, paymentRequestId as sent by RetrievePaymentHandler.java
+ * - Generates: paymentConfirmationId
+ * - Sends: taskToken, paymentRequestId, paymentConfirmationId as expected by PaymentResponseHandler.java
+ */
 export const handler = async (event: any) => {
   for (const record of event.Records) {
-    const paymentConfirmationId = uuidv4();
-    // Logic here...
+    // Parse incoming message
+    const incomingBody = JSON.parse(record.body)
+
+    // Generate ID
+    const paymentConfirmationId = uuidv4()
+
+    // Construct response using incoming data
+    const outgoingPayload = {
+      paymentConfirmationId: paymentConfirmationId,
+      taskToken: incomingBody.taskToken,
+      paymentRequestId: incomingBody.paymentRequestId
+    }
+
+    // Send :)
     await sqsClient.send(new SendMessageCommand({
       QueueUrl: QUEUE_URL,
-      MessageBody: JSON.stringify({ paymentConfirmationId })
+      MessageBody: JSON.stringify(outgoingPayload)
     }));
   }
 };
