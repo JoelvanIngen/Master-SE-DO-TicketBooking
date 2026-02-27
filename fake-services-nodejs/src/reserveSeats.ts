32def83477a658414ed482958ae3c8dc43564c1d
Joel van Ingen <j.c.ingen@gmail.com>
2026-02-27 09:58:31 +0100

    gracious seat allocation failure

--- fake-services-nodejs/src/reserveSeats.ts
+++ fake-services-nodejs/src/reserveSeats.ts
@@ -1,7 +1,10 @@
 export const handler = async (event: any) => {
     console.log("Reserving seats...", event);
     if (event.simulateBookingFailure === "seats") {
-      throw new Error("ErrorSeatsNotAvailable");
+        const error = new Error("Seats not available");
+        // Make sure the ASL error state matches (gracious failure)
+        error.name = "ErrorSeatsNotAvailable";
+        throw error;
     }
     return { reservationId: "1234" };
   };
