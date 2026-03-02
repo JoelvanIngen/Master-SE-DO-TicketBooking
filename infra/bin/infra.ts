2792e4b6786f462ea8898c9d36d9cc409fa06059
Joel van Ingen <j.c.ingen@gmail.com>
2026-03-02 22:51:17 +0100

    split deployments into prod and pr

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,6 +3,10 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TicketBookingStack } from '../lib/stack';
 
+// STACK_ENV set by github actions if PR
+const envName = process.env.STACK_ENV || 'Prod';
+const stackName = `TicketBookingStack-${envName}`;
+
 const app = new cdk.App();
 new TicketBookingStack(app, 'TicketBookingStack', {
     env: {
