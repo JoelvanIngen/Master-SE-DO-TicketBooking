5e4ba6f0509a34eb5f7883305a5d6c3bcdf136ac
Joël van Ingen <j.c.ingen@gmail.com>
2026-03-03 09:41:31 +0100

    Revert "Revert "60 implement infrastructure as code""

---
+++ infra/bin/infra.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { TicketBookingStack } from '../lib/stack';
+
+// STACK_ENV set by github actions if PR
+const envName = process.env.STACK_ENV || 'Prod';
+const stackName = `TicketBookingStack-${envName}`;
+
+const app = new cdk.App();
+new TicketBookingStack(app, stackName, {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: 'eu-north-1'
+    },
+});
