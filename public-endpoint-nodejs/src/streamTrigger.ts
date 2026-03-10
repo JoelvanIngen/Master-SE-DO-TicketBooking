88425961a9f94565ee0d49a09c176b6f35ca5f6d
Joel van Ingen <j.c.ingen@gmail.com>
2026-03-10 09:35:18 +0100

    add logging to streamtrigger

--- public-endpoint-nodejs/src/streamTrigger.ts
+++ public-endpoint-nodejs/src/streamTrigger.ts
@@ -6,19 +6,31 @@ export const handler = async (event: any) => {
   // If the start is requested in the public endpoint, there might be cases where the machine failed
   // to start only after the database was already written
   // => causing ghost records
+  console.log(`Processing ${event.Records.length} records from stream.`);
   for (const record of event.Records) {
     // Only trigger on new records (INSERT)
     if (record.eventName === 'INSERT') {
-      const newItem = record.dynamodb.NewImage;
-      const bookingReferenceId = newItem.bookingReferenceId.S;
-      const simulateBookingFailure = newItem.simulateBookingFailure.S;
+      try {
+        const newItem = record.dynamodb.NewImage;
+        const bookingReferenceId = newItem.bookingReferenceId.S;
+        const simulateBookingFailure = newItem.simulateBookingFailure.S;
 
-      await sfnClient.send(
-        new StartExecutionCommand({
-          stateMachineArn: process.env.STATE_MACHINE_ARN,
-          input: JSON.stringify({ bookingReferenceId, simulateBookingFailure }),
-        }),
-      );
+        const result = await sfnClient.send(
+          new StartExecutionCommand({
+            stateMachineArn: process.env.STATE_MACHINE_ARN,
+            input: JSON.stringify({ bookingReferenceId, simulateBookingFailure }),
+          }),
+        );
+
+        console.log(`SFN Started. Execution Arn: ${result.executionArn}`);
+      } catch (err: any) {
+        console.error('FAILED to process record:');
+        console.error('Error:', err.message);
+        console.error('Stack:', err.stack);
+        throw err;
+      }
+    } else {
+      console.log(`Skipping record: ${record.eventName}`);
     }
   }
 };
